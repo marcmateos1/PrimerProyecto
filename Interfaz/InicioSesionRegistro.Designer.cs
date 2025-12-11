91d3d41eef83b99632069cff3cb1b844ad9ff7b6
Pau <paufc06@gmail.com>
2025-12-11 01:17:12 +0100

    companyies a la interfaç

--- Interfaz/InicioSesionRegistro.Designer.cs
+++ Interfaz/InicioSesionRegistro.Designer.cs
@@ -46,8 +46,30 @@
             textUsuario = new TextBox();
             labelInicioSesion = new Label();
             label1 = new Label();
+            entraDades = new Panel();
+            label7 = new Label();
+            logInMail = new TextBox();
+            label4 = new Label();
+            anarAreg = new Button();
+            button2 = new Button();
+            logInTel = new TextBox();
+            label5 = new Label();
+            logInNom = new TextBox();
+            label6 = new Label();
+            RegEmpresa = new Panel();
+            label8 = new Label();
+            regMail = new TextBox();
+            label9 = new Label();
+            Cancelar = new Button();
+            registrar = new Button();
+            regTel = new TextBox();
+            label10 = new Label();
+            regNom = new TextBox();
+            label11 = new Label();
             panelRegistro.SuspendLayout();
             panelInicioSesion.SuspendLayout();
+            entraDades.SuspendLayout();
+            RegEmpresa.SuspendLayout();
             SuspendLayout();
             // 
             // panelRegistro
@@ -68,11 +90,11 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Font = new Font("Rubik", 11.999999F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Font = new Font("Microsoft Sans Serif", 11.999999F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label2.Location = new Point(122, 44);
             label2.Name = "label2";
             label2.RightToLeft = RightToLeft.Yes;
-            label2.Size = new Size(112, 24);
+            label2.Size = new Size(122, 25);
             label2.TabIndex = 6;
             label2.Text = "REGISTRO";
             // 
@@ -146,10 +168,10 @@
             // label3
             // 
             label3.AutoSize = true;
-            label3.Font = new Font("Rubik", 11.999999F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Font = new Font("Microsoft Sans Serif", 11.999999F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label3.Location = new Point(91, 44);
             label3.Name = "label3";
-            label3.Size = new Size(153, 24);
+            label3.Size = new Size(166, 25);
             label3.TabIndex = 6;
             label3.Text = "INICIO SESIÓN";
             // 
@@ -217,11 +239,204 @@
             label1.TabIndex = 2;
             label1.Text = "BIENVENIDO";
             // 
+            // entraDades
+            // 
+            entraDades.Controls.Add(label7);
+            entraDades.Controls.Add(logInMail);
+            entraDades.Controls.Add(label4);
+            entraDades.Controls.Add(anarAreg);
+            entraDades.Controls.Add(button2);
+            entraDades.Controls.Add(logInTel);
+            entraDades.Controls.Add(label5);
+            entraDades.Controls.Add(logInNom);
+            entraDades.Controls.Add(label6);
+            entraDades.Location = new Point(50, 439);
+            entraDades.Name = "entraDades";
+            entraDades.Size = new Size(327, 294);
+            entraDades.TabIndex = 7;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(53, 198);
+            label7.Name = "label7";
+            label7.Size = new Size(46, 20);
+            label7.TabIndex = 8;
+            label7.Text = "eMail";
+            // 
+            // logInMail
+            // 
+            logInMail.Location = new Point(155, 191);
+            logInMail.Name = "logInMail";
+            logInMail.Size = new Size(125, 27);
+            logInMail.TabIndex = 7;
+            logInMail.UseSystemPasswordChar = true;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Microsoft Sans Serif", 11.999999F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(17, 44);
+            label4.Name = "label4";
+            label4.Size = new Size(290, 25);
+            label4.TabIndex = 6;
+            label4.Text = "ENTRAR DATOS EMPRESA";
+            // 
+            // anarAreg
+            // 
+            anarAreg.Location = new Point(161, 253);
+            anarAreg.Name = "anarAreg";
+            anarAreg.Size = new Size(153, 29);
+            anarAreg.TabIndex = 5;
+            anarAreg.Text = "Dar de Alta";
+            anarAreg.UseVisualStyleBackColor = true;
+            anarAreg.Click += anarAreg_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(17, 253);
+            button2.Name = "button2";
+            button2.Size = new Size(124, 29);
+            button2.TabIndex = 4;
+            button2.Text = "Entrar Datos";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // logInTel
+            // 
+            logInTel.Location = new Point(155, 140);
+            logInTel.Name = "logInTel";
+            logInTel.Size = new Size(125, 27);
+            logInTel.TabIndex = 3;
+            logInTel.UseSystemPasswordChar = true;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(48, 140);
+            label5.Name = "label5";
+            label5.Size = new Size(70, 20);
+            label5.TabIndex = 2;
+            label5.Text = "Teléfono:";
+            // 
+            // logInNom
+            // 
+            logInNom.Location = new Point(155, 93);
+            logInNom.Name = "logInNom";
+            logInNom.Size = new Size(125, 27);
+            logInNom.TabIndex = 1;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(56, 93);
+            label6.Name = "label6";
+            label6.Size = new Size(67, 20);
+            label6.TabIndex = 0;
+            label6.Text = "Nombre:";
+            // 
+            // RegEmpresa
+            // 
+            RegEmpresa.Controls.Add(label8);
+            RegEmpresa.Controls.Add(regMail);
+            RegEmpresa.Controls.Add(label9);
+            RegEmpresa.Controls.Add(Cancelar);
+            RegEmpresa.Controls.Add(registrar);
+            RegEmpresa.Controls.Add(regTel);
+            RegEmpresa.Controls.Add(label10);
+            RegEmpresa.Controls.Add(regNom);
+            RegEmpresa.Controls.Add(label11);
+            RegEmpresa.Location = new Point(424, 439);
+            RegEmpresa.Name = "RegEmpresa";
+            RegEmpresa.Size = new Size(327, 294);
+            RegEmpresa.TabIndex = 9;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(69, 198);
+            label8.Name = "label8";
+            label8.Size = new Size(46, 20);
+            label8.TabIndex = 8;
+            label8.Text = "eMail";
+            // 
+            // regMail
+            // 
+            regMail.Location = new Point(155, 191);
+            regMail.Name = "regMail";
+            regMail.Size = new Size(125, 27);
+            regMail.TabIndex = 7;
+            regMail.UseSystemPasswordChar = true;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Microsoft Sans Serif", 11.999999F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label9.Location = new Point(69, 44);
+            label9.Name = "label9";
+            label9.Size = new Size(196, 25);
+            label9.TabIndex = 6;
+            label9.Text = "NUEVA EMPRESA";
+            // 
+            // Cancelar
+            // 
+            Cancelar.Location = new Point(171, 253);
+            Cancelar.Name = "Cancelar";
+            Cancelar.Size = new Size(137, 29);
+            Cancelar.TabIndex = 5;
+            Cancelar.Text = "Cancelar Registro";
+            Cancelar.UseVisualStyleBackColor = true;
+            // 
+            // registrar
+            // 
+            registrar.Location = new Point(31, 253);
+            registrar.Name = "registrar";
+            registrar.Size = new Size(124, 29);
+            registrar.TabIndex = 4;
+            registrar.Text = "Dar de alta";
+            registrar.UseVisualStyleBackColor = true;
+            registrar.Click += registrar_Click;
+            // 
+            // regTel
+            // 
+            regTel.Location = new Point(155, 140);
+            regTel.Name = "regTel";
+            regTel.Size = new Size(125, 27);
+            regTel.TabIndex = 3;
+            regTel.UseSystemPasswordChar = true;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(48, 140);
+            label10.Name = "label10";
+            label10.Size = new Size(70, 20);
+            label10.TabIndex = 2;
+            label10.Text = "Teléfono:";
+            // 
+            // regNom
+            // 
+            regNom.Location = new Point(155, 93);
+            regNom.Name = "regNom";
+            regNom.Size = new Size(125, 27);
+            regNom.TabIndex = 1;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(56, 93);
+            label11.Name = "label11";
+            label11.Size = new Size(67, 20);
+            label11.TabIndex = 0;
+            label11.Text = "Nombre:";
+            // 
             // InicioSesionRegistro
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(800, 821);
+            Controls.Add(RegEmpresa);
+            Controls.Add(entraDades);
             Controls.Add(label1);
             Controls.Add(panelInicioSesion);
             Controls.Add(panelRegistro);
@@ -231,6 +446,10 @@
             panelRegistro.PerformLayout();
             panelInicioSesion.ResumeLayout(false);
             panelInicioSesion.PerformLayout();
+            entraDades.ResumeLayout(false);
+            entraDades.PerformLayout();
+            RegEmpresa.ResumeLayout(false);
+            RegEmpresa.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -257,5 +476,25 @@
         private Label label1;
         private Label label2;
         private Label label3;
+        private Panel entraDades;
+        private Label label7;
+        private TextBox logInMail;
+        private Label label4;
+        private Button anarAreg;
+        private Button button2;
+        private TextBox logInTel;
+        private Label label5;
+        private TextBox logInNom;
+        private Label label6;
+        private Panel RegEmpresa;
+        private Label label8;
+        private TextBox regMail;
+        private Label label9;
+        private Button Cancelar;
+        private Button registrar;
+        private TextBox regTel;
+        private Label label10;
+        private TextBox regNom;
+        private Label label11;
     }
 }
